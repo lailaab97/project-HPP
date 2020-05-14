c48c66663ece9a87f0bdc57aa6668c95e0947352
mariam9498 <mm.bouhriz@gmail.com>
2020-05-14 17:03:12 +0200

    Updated main class to call processing methods

--- projetHpp/src/main/java/projetHpp/MainClass.java
+++ projetHpp/src/main/java/projetHpp/MainClass.java
@@ -1,5 +1,35 @@
 package projetHpp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import parsing.DataParsing;
+
 public class MainClass {
+	
+	
+	public static void main(String[] args) { 
+		
+		DataParsing parser = new DataParsing();
+		
+		File myDirectory = new File("/Users/macpro/Downloads/data_hpp/20");
+		//File[] containingFiles = myDirectory.listFiles();
+		String[] containingFilesNames = myDirectory.list();
+		
+		for (String fileName : containingFilesNames) {
+			  if (fileName.matches("(France|Italy|Spain)+.csv")) {
+				   try {
+
+					parser.fetchCsvFileData(myDirectory,fileName);
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			   }
+
+
+		}
+	}
+
 
 }
