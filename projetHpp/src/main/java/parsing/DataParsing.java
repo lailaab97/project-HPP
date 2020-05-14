713ad39c8787d05acdcf8f53bac9584d3bd10875
mariam9498 <mm.bouhriz@gmail.com>
2020-05-14 17:04:12 +0200

    Updated Parsing input data files

--- projetHpp/src/main/java/parsing/DataParsing.java
+++ projetHpp/src/main/java/parsing/DataParsing.java
@@ -12,61 +12,88 @@ import structure.Tree;
 
 
 /**
- * DataParsing reads csv input files and calls suitable methods to process the generated data
+ * DataParsing reads CSV input files and calls suitable methods to process the generated data
  * 
  * @author Team
  * 
  */
 public class DataParsing {
+
 	
+	/**
+	 * This method Reads and Parses CSV files
+	 * @param directory: directory of input data, file: file name
+	 * @return
+	 */
 	public void fetchCsvFileData(File directory,String file) throws FileNotFoundException {
 		
 		
 		ArrayList<Tree> mainListOfResults = new ArrayList<Tree>();
- 
-        final String delimiter = ", ";
-		
+ 	
         
         FileReader fr = new FileReader(directory+"/"+file);
         BufferedReader br = new BufferedReader(fr);
 		      
 		String line = "";
 		String[] infos;
-				           
+        String delimiter = ", ";		           
 				           
 		try {				           
 			System.out.println(file.toString());     
 			while((line = br.readLine()) != null) {
 				infos = line.split(delimiter);
+				
+				int person_id;
+				int diagnosed_ts;
+				int contaminated_by ;
+				String country;
+				int score;
 
+				// if the contaminer is unknown
 				if(infos[5].contentEquals("unknown")){
-					Person victim = new Person(Integer.parseInt(infos[0]),Math.round(Float.parseFloat(infos[4])),-1,file.replace(".csv", ""),10);
-					System.out.println(victim.toString());     
 					
+					person_id = Integer.parseInt(infos[0]);
+					diagnosed_ts = Math.round(Float.parseFloat(infos[4]));
+					contaminated_by = -1;
+					country = file.replace(".csv", "");
+					score = 10;
+					Person victim = new Person(person_id,diagnosed_ts,contaminated_by,country,score);
+					
+					//System.out.println(victim.toString());     
+					//TODO
 					//process(victim, mainListOfResults);
 					//generate()
+					
+					
 				}else {
-					Person victim = new Person(Integer.parseInt(infos[0]),Math.round(Float.parseFloat(infos[4])),Integer.parseInt(infos[5]),file.replace(".csv", ""),10);
+					person_id = Integer.parseInt(infos[0]);
+					diagnosed_ts = Math.round(Float.parseFloat(infos[4]));
+					contaminated_by = Integer.parseInt(infos[5]);
+					country = file.replace(".csv", "");
+					score = 10;
+					Person victim = new Person(person_id,diagnosed_ts,contaminated_by,country,score);
+					
+					//TODO
 					//process(victim, mainListOfResults);
+					//generate()
 				}
 		            
 			  }
-			  br.close();
+			  
+			br.close();
 			            
         	} catch (FileNotFoundException e) {
         		e.printStackTrace();
             } catch(IOException ioe) {
                ioe.printStackTrace();
             }
-				   
-					   
+				   			   
 			   
 			}
 		
 	public void StoreResultData(File directory,String file) throws FileNotFoundException {
 		
 	}
-	
 	
 
 }
