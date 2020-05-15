9be37b518fbb4b54011d84771c511fe19d552c17
mariam9498 <mm.bouhriz@gmail.com>
2020-05-15 16:11:45 +0200

    Added store in csv file feature

--- projetHpp/src/main/java/parsing/DataParsing.java
+++ projetHpp/src/main/java/parsing/DataParsing.java
@@ -1,10 +1,13 @@
 package parsing;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -16,6 +19,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import beans.Person;
 import structure.Tree;
@@ -31,6 +36,9 @@ import processing.Process;
 public class DataParsing {
 	
 	private static final Logger logger = Logger.getLogger(DataParsing.class.getName()); //Logger pour afficher les erreurs
+	
+	
+	Map<Person, Integer> mapOfIdsAndScores = new HashMap<Person, Integer>(); 
 
 	
 	/**
@@ -39,13 +47,13 @@ public class DataParsing {
 	 * @return
 	 */
 	
-	@SuppressWarnings("null")
 	public void fetchCsvFileData(File directory,String file) throws FileNotFoundException {
 		
 		List<Tree> mainListOfResults = new ArrayList<Tree>();
-		Map<Person, Integer> mapOfIdsAndScores = new HashMap<Person, Integer>(); 
 		Process processLine = new Process() ;
         
+
+		
         FileReader fr = new FileReader(directory+"/"+file);
         BufferedReader br = new BufferedReader(fr);
 		      
@@ -63,50 +71,63 @@ public class DataParsing {
 			String country;
 			int score;
 			int cpt = 0;
-			while((line = br.readLine()) != null) {
-				cpt++;
-				infos = line.split(delimiter);
+			
+			    	
+					FileWriter fw = new FileWriter(directory+"/"+"output.csv", true);
+			        BufferedWriter bw = new BufferedWriter(fw); 
+
+
+					
+					while((line = br.readLine()) != null) {
+						cpt++;
+						infos = line.split(delimiter);
+						
+		
+		
+						// if the contaminer is unknown
+						if(infos[5].contentEquals("unknown")){
+							
+							person_id = Integer.parseInt(infos[0]);
+							diagnosed_ts = Math.round(Float.parseFloat(infos[4]));
+							contaminated_by = -1;
+							country = file.replace(".csv", "");
+							score = 10;
+							
+							// Creation of a new Person whose contaminer is unknow
+							Person victim = new Person(person_id,diagnosed_ts,contaminated_by,country,score);
 				
+							
+							mainListOfResults = processLine.process(victim, mainListOfResults);
+							mapOfIdsAndScores = processLine.generate(mainListOfResults,victim.getCountry());
+							
+							//Debug
+							System.out.println("Event "+cpt);
+		
+							
+							
+						}else {
+							person_id = Integer.parseInt(infos[0]);
+							diagnosed_ts = Math.round(Float.parseFloat(infos[4]));
+							contaminated_by = Integer.parseInt(infos[5]);
+							country = file.replace(".csv", "");
+							score = 10;
+							
+							// Creation of a new Person
+							Person victim = new Person(person_id,diagnosed_ts,contaminated_by,country,score);
+							
+							//TODO
+							mainListOfResults = processLine.process(victim, mainListOfResults);
+							mapOfIdsAndScores = processLine.generate(mainListOfResults,victim.getCountry());
+							
+							//Debug
+							System.out.println("Event "+cpt);
+		
+						}
+						
+						StoreResultData(bw, mapOfIdsAndScores);
+				            
+					  }
 
-
-				// if the contaminer is unknown
-				if(infos[5].contentEquals("unknown")){
-					
-					person_id = Integer.parseInt(infos[0]);
-					diagnosed_ts = Math.round(Float.parseFloat(infos[4]));
-					contaminated_by = -1;
-					country = file.replace(".csv", "");
-					score = 10;
-					Person victim = new Person(person_id,diagnosed_ts,contaminated_by,country,score);
-					
-					//System.out.println(victim.toString());     
-					//TODO
-					
-					mainListOfResults = processLine.process(victim, mainListOfResults);
-					mapOfIdsAndScores = processLine.generate(mainListOfResults,victim.getCountry());
-					System.out.println("Event "+cpt);
-					processLine.output(mapOfIdsAndScores);
-					//generate()
-					
-					
-				}else {
-					person_id = Integer.parseInt(infos[0]);
-					diagnosed_ts = Math.round(Float.parseFloat(infos[4]));
-					contaminated_by = Integer.parseInt(infos[5]);
-					country = file.replace(".csv", "");
-					score = 10;
-					Person victim = new Person(person_id,diagnosed_ts,contaminated_by,country,score);
-					
-					//TODO
-					mainListOfResults = processLine.process(victim, mainListOfResults);
-					mapOfIdsAndScores = processLine.generate(mainListOfResults,victim.getCountry());
-					System.out.println("Event "+cpt);
-					processLine.output(mapOfIdsAndScores);
-					//generate()
-				}
-
-		            
-			  }
 			  
 			br.close();
 			            
@@ -119,10 +140,36 @@ public class DataParsing {
 			   
 			}
 		
-	public void StoreResultData(File directory,String file) throws FileNotFoundException {
+	public void StoreResultData(BufferedWriter csvWriter,Map<Person, Integer> map){
+						
+				String dataLine = null;
+				
+				for (Map.Entry<Person, Integer> mapElement : map.entrySet()) { 
+					
+		            Person key = (Person)mapElement.getKey(); 
+		            int value = ((int)mapElement.getValue());
+		            
+		            if(dataLine == null) {
+		            	dataLine = key.getCountry() + " ," + key.getPerson_id()+ " ," + value;
+		            }else {
+			            dataLine = dataLine +"; "+ key.getCountry() + " ," + key.getPerson_id()+ " ," + value;
+		            }
+		            
+		        } 
+		
+				try {
+					csvWriter.write(dataLine+"\n");
+		    	    csvWriter.flush();
+		    	    
+					//Debug
+		    	    System.out.println(dataLine);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+
 		
 	}
-	
+
 	
 	 /** 
 	  * Récupere le chemin du jeu de données
